ad2d3a7b11e718ae66ee2ed984ea12b3d36504ad
volsax <91995988+volsax@users.noreply.github.com>
2021-10-08 18:53:48 -0400

    Update pa1.c

--- pa1.c
+++ pa1.c
@@ -5,22 +5,25 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
     long n_comp = 0;
     Node *head = NULL;
 
     if(strcmp("-l", argv[1])==0){
         head = List_Load_From_File(argv[2]);
+        if(head == NULL){
+            printf("NO this file");
+            return EXIT_FAILURE;
+        }
 
         head = List_Shellsort(head, &n_comp);
 
         int b = List_Save_To_File(argv[3], head);
         printf("Element save to file: %d\n", b);
-
     }
     
-    if(strcmp("-a", argv[1])==0){
-        long *array;
+    else if(strcmp("-a", argv[1])==0){
+        long *array = NULL;
         int size = 0;
 
         array = Array_Load_From_File(argv[2], &size);
